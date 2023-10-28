cb7556ed0f187b5a328725a298e3cc4b674ca856
Andre Derjagin PC <derjagin.andre@gmail.com>
2023-10-28 17:58:48 +0200

    centered components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
-import Header from '@/components/Header';
+import Header from '@/components/header';
 import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -17,7 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		<html lang="en">
 			<body className={inter.className}>
 				<Header />
-				<main>
+				<main className="max-w-[960px] mx-auto">
 					{children}
 					<Toaster />
 				</main>
