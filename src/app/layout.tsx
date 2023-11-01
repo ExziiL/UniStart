999dc634386dc4f131819492be5f162f4dd678d4
Andre Derjagin <derjagin.andre@gmail.com>
2023-11-01 19:11:54 +0100

    fixed build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
-import Header from '@/components/header';
+import Header from '@/frontend/components/header';
 import { Toaster } from '@/frontend/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
