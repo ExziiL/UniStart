e83b92fde3f7d84bd19b8c3b2b7e1cb641ea402f
Andre Derjagin <derjagin.andre@gmail.com>
2023-11-07 22:58:17 +0100

    added invalid slug message, in case of invalid url

--- src/app/vorlesungen/[vorlesung]/page.tsx
+++ src/app/vorlesungen/[vorlesung]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { VORLESUNGEN } from '@/frontend/constants/vorlesungen';
+import Link from 'next/link';
 
 interface PageProps {
 	params: {
@@ -9,10 +10,23 @@ interface PageProps {
 }
 
 function Page({ params }: PageProps) {
-	const correctVorlesung = VORLESUNGEN.find((vorlesung) => vorlesung.slug === params.vorlesung);
+	const vorlesung = VORLESUNGEN.find((vorlesung) => vorlesung.slug === params.vorlesung);
 
-	console.log(params);
-	return <div className="">{correctVorlesung ? correctVorlesung.name : 'vorlesung not found'}</div>;
+	if (!vorlesung) {
+		// TODO: add (custom) 404 page
+		return (
+			<>
+				<h1 className="text-xl font-semibold">404 - Vorlesung Not Found</h1>
+				<p>Oops! The page you're looking for doesn't exist.</p>
+				<p>You may want to head back to the homepage. If you think something is broken, report a problem.</p>
+				<div>
+					<Link href="/">Go Home</Link>
+				</div>
+			</>
+		);
+	}
+
+	return <div className="">{vorlesung.name}</div>;
 }
 
 export default Page;
