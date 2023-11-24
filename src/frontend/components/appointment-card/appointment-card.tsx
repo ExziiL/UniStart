afc6fb3ba9a1ca6c8cf24c32962f15dca9376e3d
Andre Derjagin <derjagin.andre@gmail.com>
2023-11-24 08:06:22 +0100

    Update appointment card layout

--- src/frontend/components/appointment-card/appointment-card.tsx
+++ src/frontend/components/appointment-card/appointment-card.tsx
@@ -13,9 +13,9 @@ const AppointmentCard: FunctionComponent<AppointmentCardProps> = ({ title, date,
 	return (
 		<Card className="w-[309.33px] shadow-md">
 			<CardHeader className="p-4 pb-0">
-				<CardTitle className="text-base truncate">{title}</CardTitle>
+				<CardTitle className="text-base">{title}</CardTitle>
 			</CardHeader>
-			<CardContent className="px-4 py-2">
+			<CardContent className="px-4 pb-3 pt-1">
 				<p className="text-base font-light">{dueDay}</p>
 			</CardContent>
 			<CardFooter className="space-x-1 p-4 pt-0 text-[#64748B]">
