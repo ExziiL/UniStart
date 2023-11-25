ce2d1724593497b63dda3944026b3722a05b58ea
Andre Derjagin <derjagin.andre@gmail.com>
2023-11-25 14:25:37 +0100

    Add @radix-ui/react-switch component and changed darkmode toggle to toggle

--- src/frontend/components/theme-toggle/theme-toggle.tsx
+++ src/frontend/components/theme-toggle/theme-toggle.tsx
@@ -4,35 +4,48 @@ import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 import * as React from 'react';
 
-import { Button } from '@/frontend/components/ui/button';
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuTrigger,
-} from '@/frontend/components/ui/dropdown-menu';
+import { Switch } from '@/frontend/components/ui/switch';
+import { Label } from '../ui/label';
 
 function ThemeToggle() {
-	const { setTheme } = useTheme();
+	const [mounted, setMounted] = React.useState(false);
+	const [darkmode, setDarkmode] = React.useState(false);
+
+	const { theme, setTheme } = useTheme();
+
+	React.useEffect(() => {
+		setMounted(true);
+		setDarkmode(theme === 'dark');
+	}, [theme]);
+
+	if (!mounted) {
+		return null;
+	}
 
 	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button
-					variant="outline"
-					size="icon"
-				>
-					<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-					<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-					<span className="sr-only">Toggle theme</span>
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align="end">
-				<DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
+		<div className="flex justify-center gap-2">
+			<Label
+				htmlFor="theme-toggle"
+				className="self-center"
+			>
+				<Sun />
+			</Label>
+			<Switch
+				id="theme-toggle"
+				checked={darkmode}
+				onCheckedChange={() => {
+					setDarkmode(!darkmode);
+					setTheme(darkmode ? 'light' : 'dark');
+				}}
+			/>
+			<Label
+				htmlFor="theme-toggle"
+				className="self-center"
+			>
+				<Moon />
+			</Label>
+			<span className="sr-only">Toggle theme</span>
+		</div>
 	);
 }
 
