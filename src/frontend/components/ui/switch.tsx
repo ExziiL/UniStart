a269c0e06de27c40b8565419ab8e38d2ce4d719a
Andre Derjagin <derjagin.andre@gmail.com>
2023-11-25 15:23:33 +0100

    added framer motion to shadcn switch component

--- src/frontend/components/ui/switch.tsx
+++ src/frontend/components/ui/switch.tsx
@@ -1,14 +1,16 @@
 'use client';
 
+import { cn } from '@/lib/utils';
 import * as SwitchPrimitives from '@radix-ui/react-switch';
+import { motion } from 'framer-motion';
 import * as React from 'react';
 
-import { cn } from '@/lib/utils';
+const MotionThumb = motion(SwitchPrimitives.Thumb);
 
 const Switch = React.forwardRef<
 	React.ElementRef<typeof SwitchPrimitives.Root>,
 	React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
->(({ className, ...props }, ref) => (
+>(({ className, checked, ...props }, ref) => (
 	<SwitchPrimitives.Root
 		className={cn(
 			'peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input',
@@ -16,11 +18,20 @@ const Switch = React.forwardRef<
 		)}
 		{...props}
 		ref={ref}
+		style={{
+			justifyContent: checked ? 'flex-end' : 'flex-start',
+		}}
 	>
-		<SwitchPrimitives.Thumb
+		<MotionThumb
 			className={cn(
-				'pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0'
+				'pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0'
 			)}
+			layout
+			transition={{
+				type: 'spring',
+				stiffness: 500,
+				damping: 40,
+			}}
 		/>
 	</SwitchPrimitives.Root>
 ));
