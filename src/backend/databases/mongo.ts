4cdae9bfc373afdf4d983dffcfc9013d104b2847
Johannes Preisach <johapr01@gmial.com>
2023-11-29 15:43:36 +0100

    save dates from scrapper in MongoDB

--- src/backend/databases/mongo.ts
+++ src/backend/databases/mongo.ts
@@ -1,4 +1,5 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { MongoClient, MongoOptions, ServerApiVersion } from 'mongodb';
+import { dateScrapper } from '../scrapper/scrapper';
 const uri = "mongodb+srv://userUniStart:uniStart124@cluster0.joewohf.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -8,16 +9,31 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-async function run() {
+
+export async function getOneFromMongo(collection: string, query: Object, options: Object) {
+  await client.connect();
+
+  await client.db("uni-start").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  const db = client.db("uni-start");
+  const collect = db.collection(`${collection}`);
+
+  const result = await collect.findOne(query, options);
+
+  if (result) return {exists: result!== null, result: result }
+  else return {exists: result!== null, result: {}}
+}
+
+export async function setMongoSingleData(collection: string, data: Object) {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+
+    const db = client.db("uni-start");
+    const collect = db.collection(`${collection}`);
+
+    collect.insertOne(data);
+  } catch (error) {
+
   }
 }
-run().catch(console.dir);
--- src/frontend/components/appointment-table/appointment-table.tsx
+++ src/frontend/components/appointment-table/appointment-table.tsx
@@ -3,52 +3,19 @@ import React from 'react';
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/frontend/components/ui/table';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/frontend/components/ui/tooltip';
 import { Info } from 'lucide-react';
-import { dateScrapper } from '@/backend/scrapper/scrapper';
-
-const eintraege = [
-	{
-		title: 'Freischaltung Online-Bewerbung',
-		date: 'Mo, 10.07.2023',
-		tooltip: 'Online-Bewerbung freigeschaltet',
-	},
-	{
-		title: 'Infoveranstaltung für Masterstudiengänge (online)',
-		date: 'Mi, 12.07.2023',
-	},
-	{
-		title: 'Infoveranstaltung für Bachelorstudiengänge (Präsenz)',
-		date: 'Sa, 15.07.2023',
-	},
-	{
-		title: 'Bewerbungsschluss für Masterstudiengänge',
-		date: 'Sa, 15.07.2023',
-		tooltip: 'Ausschlussfrist! (ggf. Änderungen – siehe Zulassungssatzung)',
-	},
-	{
-		title: 'Bewerbungsschluss für NC-Studiengänge (Bachelor) und höhere Semester',
-		date: 'Di, 06.10.2023',
-	},
-	{
-		title: 'Vorlage aktueller Notenspiegel (Wechsler höheres Semester)',
-		date: 'Do, 07.10.2023',
-	},
-	{
-		title: 'Letzter Termin zur Immatrikulation bei NC-freien Bachelorstudiengängen und höheren Semestern',
-		date: 'So, 13.10.2023',
-	},
-	{
-		title: 'Letzter Termin zur Immatrikulation bei Masterstudiengängen',
-		date: 'Mi, 10.11.2023',
-	},
-	{
-		title: 'Letzter Abgabetermin Anerkennungen',
-		date: 'Fr, 12.12.2023',
-	},
-];
+import { Information, dateScrapper } from '@/backend/scrapper/scrapper';
+import { getOneFromMongo, setMongoSingleData } from '@/backend/databases/mongo';
 
 const AppointmentTable = async () => {
+	let data = await getOneFromMongo('scrapper', { semester: 'WS23' }, { projection: { table: 1 } })
+	
+	if (!data.exists) {
+		console.log("Not n database found");
+		setMongoSingleData('scrapper', { semester: 'WS23', table: await dateScrapper() })
+		data = await getOneFromMongo('scrapper', { semester: 'WS23' }, { projection: {  table: 1 } });
+	}
 
-	const entries = await dateScrapper();
+	const entries: Information[] = Object.values(data.result)[1];
 
 	return (
 		<Table>
@@ -82,7 +49,6 @@ const AppointmentTable = async () => {
 									</TooltipProvider>
 								)}
 							</TableCell>
-
 							<TableCell className="text-right min-w-[140px]">{entry.dates[0]}</TableCell>
 						</TableRow>
 					);
