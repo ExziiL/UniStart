557a779d1e10c24748b08a09c1c06ef8494a6c8f
Andre Derjagin <derjagin.andre@gmail.com>
2023-11-29 21:54:14 +0100

    Add user activity status component and import it in user bar component

---
+++ src/frontend/components/user-activity-status/user-activity-status.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function UserActivityStatus() {
+	return <div>UserActivityStatus</div>;
+}
+
+export default UserActivityStatus;
--- src/frontend/components/user-bar/user-bar.tsx
+++ src/frontend/components/user-bar/user-bar.tsx
@@ -1,9 +1,14 @@
+import UserActivityStatus from '@/frontend/components/user-activity-status/user-activity-status';
+import { Settings } from 'lucide-react';
 import React from 'react';
 
 function UserBar() {
 	return (
 		<div>
-			<div className="h-16 border-b">Hello?</div>
+			<div className="flex h-16 items-center justify-between border-b p-2">
+				<UserActivityStatus />
+				<Settings />
+			</div>
 		</div>
 	);
 }
