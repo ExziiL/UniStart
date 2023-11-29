d45bb063482ad5473ffcdd3cfe80dcb7800845b5
Andre Derjagin <derjagin.andre@gmail.com>
2023-11-29 21:48:50 +0100

    Add Chat page and UserBar component

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,12 @@
+import UserBar from '@/frontend/components/user-bar/user-bar';
+import React from 'react';
+
+function Chat() {
+	return (
+		<div>
+			<UserBar />
+		</div>
+	);
+}
+
+export default Chat;
---
+++ src/frontend/components/user-bar/user-bar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserBar() {
+	return (
+		<div>
+			<div className="h-16 border-b">Hello?</div>
+		</div>
+	);
+}
+
+export default UserBar;
