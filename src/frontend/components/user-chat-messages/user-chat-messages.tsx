e9a2d6bac50a35e6ca44538466d4aef25ade33d8
Andre Derjagin <derjagin.andre@gmail.com>
2023-12-13 15:43:51 +0100

    Add new chat button to user chat messages component

--- src/frontend/components/user-chat-messages/user-chat-messages.tsx
+++ src/frontend/components/user-chat-messages/user-chat-messages.tsx
@@ -1,21 +1,30 @@
 import { Separator } from '@/frontend/components/ui/separator';
 import UserChatSingleMessage from '@/frontend/components/user-chat-single-message';
 import { USERS } from '@/frontend/constants/users';
+import { Edit } from 'lucide-react';
 import React from 'react';
 
 function UserChatMessages() {
 	return (
 		<div className="w-96 bg-zinc-50">
-			{USERS.map((user, index) => (
-				<div key={user.id}>
-					<UserChatSingleMessage user={user} />
-					{index !== USERS.length - 1 && (
-						<div className="mx-4">
-							<Separator />
-						</div>
-					)}
+			<div className="flex items-end justify-between p-4 pb-2 text-lg font-semibold">
+				<h2 className="">Messages</h2>
+				<div className="cursor-pointer text-zinc-500 transition-colors hover:text-primary">
+					<Edit />
 				</div>
-			))}
+			</div>
+			<div className="">
+				{USERS.map((user, index) => (
+					<div key={user.id}>
+						<UserChatSingleMessage user={user} />
+						{index !== USERS.length - 1 && (
+							<div className="mx-4">
+								<Separator />
+							</div>
+						)}
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
