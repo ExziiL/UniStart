83a4526b40817d7b905400fc287c9ea9dbd05dc4
Andre Derjagin <derjagin.andre@gmail.com>
2023-12-21 08:50:11 +0100

    Add Providers component for theme management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { Providers } from './providers';
 
 import '@smastrom/react-rating/style.css';
 
@@ -18,21 +19,19 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
+		<html
+			lang="en"
+			suppressHydrationWarning
+		>
 			<body className={inter.className}>
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="system"
-					enableSystem
-					disableTransitionOnChange
-				>
+				<Providers>
 					<Navigation />
 					<main className="mx-auto max-w-[960px]">
 						{children}
 						<SpeedInsights />
 						<Toaster />
 					</main>
-				</ThemeProvider>
+				</Providers>
 			</body>
 		</html>
 	);
