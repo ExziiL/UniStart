963138ab32ee37074e2c7090e970ffac67114410
Andre Derjagin <derjagin.andre@gmail.com>
2023-12-21 15:06:15 +0100

    Add AI chat constants

---
+++ src/frontend/constants/ai-chats.ts
@@ -0,0 +1,32 @@
+export const AI_CHATS = [
+    {
+        id: 1,
+        isAi: true,
+        name: 'Hi, I am the AI chatbot',
+        messages: [
+            {
+                conversationId: 'ai_conv_1',
+                text: "Hi, I am the AI chatbot. How can I help you?",
+                date: '2023-10-10T12:00:00.000Z',
+                timestamp: 1633396800000,
+                senderId: 1,
+                receiverId: 2,
+            }
+        ]
+    },
+    {
+        id: 2,
+        isAi: true,
+        name: 'Hallo, ich bin der AI Chatbot!',
+        messages: [
+            {
+                conversationId: 'ai_conv_2',
+                text: "Hallo, ich bin der AI Chatbot. Wie kann ich dir helfen?",
+                date: '2023-10-10T12:00:00.000Z',
+                timestamp: 1633396800000,
+                senderId: 2,
+                receiverId: 3,
+            }
+        ]
+    }
+]
