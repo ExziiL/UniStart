c1eebaebbc3467eed370cc14ea40d517fe30ccb9
Andre Derjagin <derjagin.andre@gmail.com>
2023-12-21 15:05:07 +0100

    Add AiChat and AiChatMessages components

---
+++ src/app/ai-chat/page.tsx
@@ -0,0 +1,14 @@
+import AiChatMessages from '@/frontend/components/ai-chat-messages';
+import ChatBox from '@/frontend/components/chat-box';
+import React from 'react';
+
+function AiChat() {
+	return (
+		<div className="">
+			<AiChatMessages />
+			<ChatBox />
+		</div>
+	);
+}
+
+export default AiChat;
---
+++ src/frontend/components/ai-chat-messages/ai-chat-messages.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { Separator } from '@/frontend/components/ui/separator';
+import UserChatSingleMessage from '@/frontend/components/user-chat-single-message';
+import { USERS } from '@/frontend/constants/users';
+import { Edit } from 'lucide-react';
+import React from 'react';
+
+function AiChatMessages() {
+	const [activeChat, setActiveChat] = React.useState<number | undefined>(undefined);
+
+	const handleUserClick = (userId: number) => {
+		console.log(userId, ' clicked');
+		setActiveChat(userId);
+	};
+
+	return (
+		<div>
+			{' '}
+			<div className="w-96 bg-background">
+				<div className="flex items-end justify-between p-4 pb-2 text-lg font-semibold">
+					<h2 className="">Messages</h2>
+					<div className="text-primary-muted cursor-pointer transition-colors hover:text-primary">
+						<Edit />
+					</div>
+				</div>
+				<div className="">
+					{USERS.map((user, index) => (
+						<div
+							key={user.id}
+							onClick={() => handleUserClick(user.id)}
+						>
+							<UserChatSingleMessage
+								user={user}
+								activeChat={activeChat}
+							/>
+							{/* <div>{user.id}</div> */}
+							{index !== USERS.length - 1 && (
+								<div className="mx-4">
+									<Separator />
+								</div>
+							)}
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default AiChatMessages;
