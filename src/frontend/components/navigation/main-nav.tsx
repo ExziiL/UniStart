959c92a4aac92dbd1fb26895004f8032061d3e41
Andre Derjagin <derjagin.andre@gmail.com>
2023-12-23 17:53:45 +0100

    Add navigation menu to main nav component

--- src/frontend/components/navigation/main-nav.tsx
+++ src/frontend/components/navigation/main-nav.tsx
@@ -1,6 +1,17 @@
 'use client';
 
 import NavAvatar from '@/frontend/components/navigation/nav-avatar';
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuIndicator,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+	NavigationMenuViewport,
+	navigationMenuTriggerStyle,
+} from '@/frontend/components/ui/navigation-menu';
 import navItems from '@/frontend/constants/nav-items';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
@@ -20,20 +31,45 @@ const MainNav = () => {
 				<span className="hidden font-bold sm:inline-block">LOGO</span>
 			</Link>
 
-			<nav className="flex items-center space-x-6 text-sm font-medium">
-				{navItems.map((item) => (
-					<Link
-						key={item.href}
-						href={item.href}
-						className={cn(
-							'transition-colors hover:text-foreground/90',
-							pathname === item.href ? 'text-foreground' : 'text-foreground/60'
-						)}
-					>
-						{item.title}
-					</Link>
-				))}
-			</nav>
+			<NavigationMenu>
+				<Link
+					href="/"
+					legacyBehavior
+					passHref
+				>
+					<NavigationMenuLink className={navigationMenuTriggerStyle()}>Home</NavigationMenuLink>
+				</Link>
+
+				<NavigationMenuList>
+					<NavigationMenuItem>
+						<NavigationMenuTrigger>Components</NavigationMenuTrigger>
+						<NavigationMenuContent>
+							<ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+								{navItems.map((component) => (
+									<ListItem
+										key={component.title}
+										title={component.title}
+										href={component.href}
+									>
+										{component.description}
+									</ListItem>
+								))}
+							</ul>
+						</NavigationMenuContent>
+					</NavigationMenuItem>
+				</NavigationMenuList>
+
+				<Link
+					href="/ai-chat"
+					legacyBehavior
+					passHref
+				>
+					<NavigationMenuLink className={navigationMenuTriggerStyle()}>Ai-Chat</NavigationMenuLink>
+				</Link>
+			</NavigationMenu>
+
+			{/* ----------------------------------------------- */}
+
 			{/* Diese div ist dafür da um die navigation items durch das "justify-between" zentral zu halten, deshalb nicht löschen! */}
 			<NavAvatar />
 		</div>
@@ -41,3 +77,26 @@ const MainNav = () => {
 };
 
 export default MainNav;
+
+const ListItem = React.forwardRef<React.ElementRef<'a'>, React.ComponentPropsWithoutRef<'a'>>(
+	({ className, title, children, ...props }, ref) => {
+		return (
+			<li>
+				<NavigationMenuLink asChild>
+					<a
+						ref={ref}
+						className={cn(
+							'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+							className
+						)}
+						{...props}
+					>
+						<div className="text-sm font-medium leading-none">{title}</div>
+						<p className="line-clamp-2 text-sm leading-snug text-muted-foreground">{children}</p>
+					</a>
+				</NavigationMenuLink>
+			</li>
+		);
+	}
+);
+ListItem.displayName = 'ListItem';
