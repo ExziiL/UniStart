fd9146de987c4195a39cd564ad3f62032ed1cd05
Andre Derjagin <derjagin.andre@gmail.com>
2023-12-28 23:18:15 +0100

    Refactor AI_CHATS messages structure

--- src/frontend/constants/ai-chats.ts
+++ src/frontend/constants/ai-chats.ts
@@ -1,5 +1,9 @@
-export const AI_CHATS = { messages: [{"role": "system", "content": "You are a helpful assistant."},
-{"role": "user", "content": "Who won the world series in 2020?"},
-{"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-{"role": "user", "content": "Where was it played?"}],
-model: "gpt-3.5-turbo" } 
+export const AI_CHATS = {
+	messages: [
+		{ role: 'user', content: 'Who won the world series in 2020?' },
+		{ role: 'assistant', content: 'The Los Angeles Dodgers won the World Series in 2020.' },
+		{ role: 'user', content: 'Where was it played?' },
+		{ role: 'assistant', content: 'It was played somewhere...' },
+	],
+	model: 'gpt-3.5-turbo',
+};
