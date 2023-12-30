b3a1684fbf1817f9f948f5363dcab11c647c48c2
Andre Derjagin <derjagin.andre@gmail.com>
2023-12-30 20:59:53 +0100

    Add voting functionality to CourseReview component

--- src/frontend/components/course-reviews/course-review.tsx
+++ src/frontend/components/course-reviews/course-review.tsx
@@ -1,28 +1,61 @@
+import { Button } from "@/frontend/components/ui/button";
+import { Separator } from "@/frontend/components/ui/separator";
 import { Review } from "@/types/IReview";
+import { ArrowBigDown, ArrowBigUp, ThumbsDown, ThumbsUp } from "lucide-react";
 import React from "react";
 import StarRating from "../star-rating";
-import { Separator } from "../ui/separator";
 
 interface CourseReviewProps extends React.HTMLAttributes<HTMLDivElement> {
 	review: Review;
 }
 
 function CourseReview({ review }: CourseReviewProps) {
+	const upvotes = review.upvotes;
+	const downvotes = review.downvotes;
+
 	return (
-		<div className="pt-7 text-primary">
-			<h2 className="font-medium">{review.headline}</h2>
-			<div className="flex gap-3 pb-2 pt-1">
-				<StarRating rating={review.rating} />
-
-				<div className="py-1">
-					<Separator orientation="vertical" />
-				</div>
-
-				<span className="flex flex-col justify-center text-sm text-light">2 minutes ago</span>
+		<div className="flex gap-3 pt-7 text-primary">
+			<div className="flex flex-col justify-center">
+				<Voting
+					upvotes={upvotes}
+					downvotes={downvotes}
+				/>
 			</div>
-			<p className="">{review.description}</p>
+			<div>
+				<h2 className="font-medium">{review.headline}</h2>
+				<div className="flex gap-3 pb-2 pt-1">
+					<StarRating rating={review.rating} />
+
+					<div className="py-1">
+						<Separator orientation="vertical" />
+					</div>
+
+					<span className="flex flex-col justify-center text-sm text-light">
+						{review.date.toLocaleDateString("de-DE", {
+							year: "numeric",
+							month: "long",
+							day: "numeric",
+						})}
+					</span>
+				</div>
+				<p className="">{review.description}</p>
+			</div>
 		</div>
 	);
 }
+
+const Voting = ({ upvotes, downvotes }: { upvotes: number; downvotes: number }) => {
+	return (
+		<div className="flex flex-col items-center justify-center gap-1">
+			<div className="cursor-pointer text-ultra-light transition-colors hover:text-primary">
+				<ThumbsUp size={20} />
+			</div>
+			<span className=" text-lg font-medium text-primary">{upvotes - downvotes}</span>
+			<div className="cursor-pointer text-ultra-light transition-colors hover:text-primary">
+				<ThumbsDown size={20} />
+			</div>
+		</div>
+	);
+};
 
 export default CourseReview;
--- src/types/IReview.ts
+++ src/types/IReview.ts
@@ -5,4 +5,5 @@ export interface Review {
 	description: string;
 	upvotes: number;
 	downvotes: number;
+	// Hier noch den Verfasser hinzufügen, sobald wir die User aus der Datenbank haben. Wird dazu benötigt, um zu der Bewertungen das Semester und ggf. den Studiengang anzufügen. 
 }
