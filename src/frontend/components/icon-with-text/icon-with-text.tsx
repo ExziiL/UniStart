dc093027450e88babc3d8ef6046425829da0e2e2
Andre Derjagin <derjagin.andre@gmail.com>
2023-12-30 16:03:12 +0100

    Refactor IconWithText component text color

--- src/frontend/components/icon-with-text/icon-with-text.tsx
+++ src/frontend/components/icon-with-text/icon-with-text.tsx
@@ -9,18 +9,18 @@ const IconWithText = ({ text, icon }: IconWithTextProps) => {
 	const defaultIconProps = {
 		size: 20,
 		strokeWidth: 2,
-		className: "text-primary",
+		className: "",
 	};
 
 	const iconWithProps = React.cloneElement(icon as React.ReactElement, defaultIconProps);
 
 	return (
-		<div className="flex items-center gap-2">
+		<div className="flex items-center gap-2 text-light">
 			{iconWithProps}
 			{/* size={16}
 				strokeWidth={2}
 				className="text-primary" */}
-			<span className="pt-[2px] text-base font-normal text-primary">{text}</span>
+			<span className="pt-[2px] text-base font-normal">{text}</span>
 		</div>
 	);
 };
