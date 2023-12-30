82d20f7cb6abe03903194b2fb0f7b9041787d5f8
Andre Derjagin <derjagin.andre@gmail.com>
2023-12-30 21:31:56 +0100

    Refactor star-rating component imports

--- src/frontend/components/star-rating/star-rating.tsx
+++ src/frontend/components/star-rating/star-rating.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import React from "react";
-
 import { Rating, SharedProps } from "@smastrom/react-rating";
+import React from "react";
 
 interface StarRatingProps {
 	rating: number;
