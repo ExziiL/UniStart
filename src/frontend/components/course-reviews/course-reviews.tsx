d2aa04c65877e6efae306379d26e7166a34a4928
Andre Derjagin <derjagin.andre@gmail.com>
2023-12-30 22:01:34 +0100

    Add Lucide icons for sorting and filtering reviews

--- src/frontend/components/course-reviews/course-reviews.tsx
+++ src/frontend/components/course-reviews/course-reviews.tsx
@@ -3,7 +3,7 @@
 import CourseReview from "@/frontend/components/course-reviews/course-review";
 import { Button } from "@/frontend/components/ui/button";
 import { Review } from "@/types/IReview";
-import { Edit3 } from "lucide-react";
+import { ArrowUpDown, Edit3, Filter } from "lucide-react";
 import React from "react";
 import { Separator } from "../ui/separator";
 
@@ -14,8 +14,6 @@ interface CourseReviewsProps {
 function CourseReviews({ reviews }: CourseReviewsProps) {
 	const numOfReviews = reviews.length;
 
-	// TODO: Add Star with Ratings
-	// TODO: Add correct Date
 	// TODO: Make with smaller
 	// TODO: Change Star Color of small stars
 	// TODO: Add "Write a Review"-Button Modal
@@ -25,7 +23,7 @@ function CourseReviews({ reviews }: CourseReviewsProps) {
 	};
 
 	return (
-		<div>
+		<div className="flex flex-col">
 			<div className="flex justify-between">
 				<h2 className="text-2xl font-medium text-primary">
 					Reviews <span className="pl-1 text-lg font-normal text-ultra-light">({numOfReviews})</span>
@@ -42,13 +40,32 @@ function CourseReviews({ reviews }: CourseReviewsProps) {
 				</Button>
 			</div>
 
-			{reviews.map((review, index) => (
-				<div key={index}>
-					<CourseReview review={review} />
-
-					<Separator className="mt-7" />
+			<div className="flex flex-row gap-8 pb-4 pt-5">
+				<div className="flex cursor-pointer flex-row items-center gap-3">
+					<ArrowUpDown
+						size={18}
+						className=""
+					/>
+					<p>Sort</p>
 				</div>
-			))}
+				<div className="flex cursor-pointer flex-row items-center gap-3">
+					<Filter
+						size={18}
+						className=""
+					/>
+					<p>Filter</p>
+				</div>
+			</div>
+
+			<div>
+				{reviews.map((review, index) => (
+					<div key={index}>
+						<CourseReview review={review} />
+
+						<Separator className="my-7" />
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
