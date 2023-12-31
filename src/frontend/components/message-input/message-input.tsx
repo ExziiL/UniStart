ee5a3af3f1d39b010cb22d430099d77370174fce
Andre Derjagin <derjagin.andre@gmail.com>
2023-12-31 21:27:40 +0100

    Add height constraint to message-input component

--- src/frontend/components/message-input/message-input.tsx
+++ src/frontend/components/message-input/message-input.tsx
@@ -1,12 +1,27 @@
-'use client';
+"use client";
 
-import { Button } from '@/frontend/components/ui/button';
-import { Textarea } from '@/frontend/components/ui/textarea';
-import { Send } from 'lucide-react';
-import React from 'react';
+import { Button } from "@/frontend/components/ui/button";
+import { Textarea } from "@/frontend/components/ui/textarea";
+import { Send } from "lucide-react";
+import React from "react";
 
 function MessageInput() {
-	const [message, setMessage] = React.useState('');
+	const [message, setMessage] = React.useState("");
+	const textareaRef = React.useRef<HTMLTextAreaElement>(null);
+
+	const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setMessage(e.target.value);
+
+		const textarea = textareaRef.current;
+		if (textarea) {
+			textarea.style.height = "inherit"; // Shrink to fit new content
+
+			// Calculate the desired and respect the maximum height
+			const maxHeight = 102; // Set the max height in pixels
+			const desiredHeight = Math.min(textarea.scrollHeight, maxHeight);
+			textarea.style.height = `${desiredHeight}px`; // Set the new height
+		}
+	};
 
 	const handleButtonClick = () => {
 		console.log(message);
@@ -16,13 +31,18 @@ function MessageInput() {
 		<div className="">
 			<div className="flex flex-col gap-2 sm:flex-row">
 				<Textarea
+					ref={textareaRef}
 					className="resize-none"
 					placeholder="Type your message here."
 					value={message}
-					onChange={(e) => setMessage(e.target.value)}
+					onChange={handleTextChange}
 					rows={1}
 				/>
-				<Button onClick={handleButtonClick}>
+				<Button
+					onClick={handleButtonClick}
+					className="h-auto"
+					variant="outline"
+				>
 					<Send
 						strokeWidth={1.75}
 						size={20}
