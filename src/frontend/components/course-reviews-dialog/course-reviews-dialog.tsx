694eaf0b0dd6851e9fe477ca0e786ffcfc9f001d
Andre Derjagin <derjagin.andre@gmail.com>
2023-12-31 01:07:10 +0100

    Add star rating and textarea components to course reviews dialog

--- src/frontend/components/course-reviews-dialog/course-reviews-dialog.tsx
+++ src/frontend/components/course-reviews-dialog/course-reviews-dialog.tsx
@@ -21,11 +21,14 @@ import {
 	FormMessage,
 } from "@/frontend/components/ui/form";
 import { Input } from "@/frontend/components/ui/input";
+import { Textarea } from "@/frontend/components/ui/textarea";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Rating, SharedProps } from "@smastrom/react-rating";
 import { Edit3 } from "lucide-react";
 import * as React from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import StarRating from "../star-rating";
 
 interface CourseReviewsDialogProps extends React.HTMLAttributes<HTMLDivElement> {
 	// reviews: Review[];
@@ -35,6 +38,7 @@ interface CourseReviewsDialogProps extends React.HTMLAttributes<HTMLDivElement> 
 const formSchema = z.object({
 	headline: z.string().min(5).max(100),
 	description: z.string().min(5).max(1000),
+	userRating: z.number().min(1).max(5),
 });
 
 function CourseReviewsDialog({}: CourseReviewsDialogProps) {
@@ -44,13 +48,13 @@ function CourseReviewsDialog({}: CourseReviewsDialogProps) {
 		defaultValues: {
 			headline: "",
 			description: "",
+			userRating: 0,
 		},
 	});
 
 	// Handle Form Submit
 	const onSubmit = (data: z.infer<typeof formSchema>) => {
-		console.log("Form Submitted");
-		console.log(data);
+		console.log("Form Submitted: ", data);
 	};
 
 	return (
@@ -75,7 +79,28 @@ function CourseReviewsDialog({}: CourseReviewsDialogProps) {
 							<DialogTitle>Leave a Review</DialogTitle>
 							<DialogDescription>How would you rate this course?</DialogDescription>
 						</DialogHeader>
-						<div className="">
+						<div className="flex flex-col gap-4">
+							<FormField
+								control={form.control}
+								name="userRating"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Your Rating</FormLabel>
+										<FormControl>
+											<Rating
+												{...field}
+												value={field.value}
+												// className="mt-2"
+											/>
+										</FormControl>
+										<FormMessage
+											{...field}
+											className="pt-1"
+										/>
+										<FormDescription>{/* This is the Form Description */}</FormDescription>
+									</FormItem>
+								)}
+							/>
 							<FormField
 								control={form.control}
 								name="headline"
@@ -93,6 +118,26 @@ function CourseReviewsDialog({}: CourseReviewsDialogProps) {
 											className="pt-1"
 										/>
 										<FormDescription>{/* This is the Form Description */}</FormDescription>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="description"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Bio</FormLabel>
+										<FormControl>
+											<Textarea
+												placeholder="Tell us a little bit about yourself"
+												className="resize-none"
+												{...field}
+											/>
+										</FormControl>
+										<FormDescription className="pt-1">
+											You can <span>@mention</span> other users and organizations to link to them.
+										</FormDescription>
+										<FormMessage />
 									</FormItem>
 								)}
 							/>
