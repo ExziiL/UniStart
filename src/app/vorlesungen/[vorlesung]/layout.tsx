30e1d91e784617277c7f69b727fcdab854f186c6
Andre Derjagin <derjagin.andre@gmail.com>
2023-12-31 23:11:18 +0100

    Update padding in VorlesungLayout component

--- src/app/vorlesungen/[vorlesung]/layout.tsx
+++ src/app/vorlesungen/[vorlesung]/layout.tsx
@@ -1,4 +1,4 @@
 export default function VorlesungLayout({ children }: { children: React.ReactNode }) {
-	return <section className="grid-col grid grid-cols-[1fr_min(80ch,_calc(100%-32px))_1fr] pt-8">{children}</section>;
+	return <section className="grid-col grid grid-cols-[1fr_min(80ch,_calc(100%-32px))_1fr] py-8">{children}</section>;
 	// grid-template-columns: 1fr min(80ch, calc(100% - 32px)) 1fr;
 }
