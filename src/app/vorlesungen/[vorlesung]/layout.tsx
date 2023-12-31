1d8276a142513eb7f9400a11ce847d119d1ef5fc
Andre Derjagin <derjagin.andre@gmail.com>
2023-12-31 15:39:01 +0100

    Update VorlesungLayout component styling

--- src/app/vorlesungen/[vorlesung]/layout.tsx
+++ src/app/vorlesungen/[vorlesung]/layout.tsx
@@ -1,3 +1,3 @@
 export default function VorlesungLayout({ children }: { children: React.ReactNode }) {
-	return <section className="wrapper">{children}</section>;
+	return <section className="wrapper pt-8">{children}</section>;
 }
