d59991f6471fa0ee6b2ce6e7aa96ed01417eb072
Andre Derjagin <derjagin.andre@gmail.com>
2024-01-04 15:50:09 +0100

    Add FilterIcon component and handle filter change event

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,18 +1,27 @@
+import FilterIcon from "@/frontend/components/filter-icon";
 import GoogleMap from "@/frontend/components/google-map";
 import LocationList from "@/frontend/components/location-list";
 import { Input } from "@/frontend/components/ui/input";
 import { ScrollArea, ScrollBar } from "@/frontend/components/ui/scroll-area";
+import { VORLESUNGEN } from "@/frontend/constants/vorlesungen";
 import React from "react";
 
 function Map() {
+	const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		console.log(e.target.value);
+	};
+
 	return (
 		<div className="full-bleed h-[calc(100vh-58px)] p-4 md:p-24 ">
 			<div>
-				<h1>Find a Location</h1>
-				<Input
-					type="email"
-					placeholder="Email"
-				/>
+				<div className="">
+					<h1>Find a Location</h1>
+					<Input
+						type="email"
+						placeholder="Email"
+					/>
+				</div>
+				<FilterIcon onChange={handleFilterChange} />
 			</div>
 			<div className="grid h-full grid-rows-[auto,_1fr] gap-4 lg:grid-cols-[auto,_1fr] lg:grid-rows-1">
 				<ScrollArea>
--- src/frontend/components/filter-icon/filter-icon.tsx
+++ src/frontend/components/filter-icon/filter-icon.tsx
@@ -1,20 +1,91 @@
+"use client";
+
+import { Button } from "@/frontend/components/ui/button";
+import {
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerDescription,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from "@/frontend/components/ui/drawer";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/frontend/components/ui/dropdown-menu";
 import { Filter } from "lucide-react";
 import * as React from "react";
+import { useMediaQuery } from "usehooks-ts";
 
 interface FilterIconProps extends React.HTMLAttributes<HTMLDivElement> {
-	data: any;
+	data?: any;
 }
 
 function FilterIcon({ data }: FilterIconProps) {
-	return (
-		<div className="flex cursor-pointer flex-row items-center gap-3">
-			<Filter
-				size={18}
-				className=""
-			/>
-			<p>Filter</p>
-		</div>
-	);
+	const isMobile = useMediaQuery("(max-width: 768px)");
+
+	return <div>{isMobile ? <MobileFilterMenu /> : <FilterMenu />}</div>;
 }
 
 export default FilterIcon;
+
+const FilterButton = () => {
+	return (
+		<Button
+			variant="outline"
+			className="flex w-fit cursor-pointer flex-row items-center"
+		>
+			<Filter
+				size={16}
+				className="mr-2"
+			/>
+			<p>Filter</p>
+		</Button>
+	);
+};
+
+const FilterMenu = () => {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger>
+				<FilterButton />
+			</DropdownMenuTrigger>
+			<DropdownMenuContent>
+				<DropdownMenuLabel>My Account</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem>Profile</DropdownMenuItem>
+				<DropdownMenuItem>Billing</DropdownMenuItem>
+				<DropdownMenuItem>Team</DropdownMenuItem>
+				<DropdownMenuItem>Subscription</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+};
+
+const MobileFilterMenu = () => {
+	return (
+		<Drawer>
+			<DrawerTrigger>
+				<FilterButton />
+			</DrawerTrigger>
+			<DrawerContent>
+				<DrawerHeader>
+					<DrawerTitle>Are you sure absolutely sure?</DrawerTitle>
+					<DrawerDescription>This action cannot be undone.</DrawerDescription>
+				</DrawerHeader>
+				<DrawerFooter>
+					<Button>Submit</Button>
+					<DrawerClose>
+						<Button variant="outline">Cancel</Button>
+					</DrawerClose>
+				</DrawerFooter>
+			</DrawerContent>
+		</Drawer>
+	);
+};
