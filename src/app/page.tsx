7f52f21c7fb596466c7961fac82b43941342c2c0
Andre Derjagin <derjagin.andre@gmail.com>
2024-01-09 17:25:01 +0100

    Refactor feature card component and update home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,49 @@ import { Button } from "@/frontend/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
+const features = [
+	{
+		title: "Course Overviews",
+		image: "https://source.unsplash.com/random",
+		alt: "Picture of the author",
+	},
+	{
+		title: "Course Reviews",
+		image: "https://source.unsplash.com/random",
+		alt: "Picture of the author",
+	},
+	{
+		title: "Locations",
+		image: "https://source.unsplash.com/random",
+		alt: "Picture of the author",
+	},
+	{
+		title: "Chat",
+		image: "https://source.unsplash.com/random",
+		alt: "Picture of the author",
+	},
+	{
+		title: "Ai-Chat",
+		image: "https://source.unsplash.com/random",
+		alt: "Picture of the author",
+	},
+	{
+		title: "Frequently Asked Questions",
+		image: "https://source.unsplash.com/random",
+		alt: "Picture of the author",
+	},
+];
+
 export default function Home() {
 	return (
-		<div className="flex flex-col gap-16 py-16">
-			<div className="flex flex-row">
-				<div>
-					<h1>Introducing your University Helper</h1>
-					<p>Feeling Lost? Our Application helps you finding your place in on your university campus</p>
-					<Button>Get started</Button>
+		<div className="flex flex-col gap-32 py-16">
+			<div className="flex flex-col gap-16 md:flex-row">
+				<div className="flex flex-col">
+					<h1 className="text-4xl font-medium">Introducing your University Helper</h1>
+					<p className="pb-8 pt-4 text-light">
+						Feeling Lost? Our Application helps you finding your place in on your university campus
+					</p>
+					<Button className="w-fit">Get started</Button>
 				</div>
 				<Image
 					src="https://source.unsplash.com/random"
@@ -21,34 +56,20 @@ export default function Home() {
 				/>
 			</div>
 
-			<div className="fler-row flex gap-4">
-				<FeatureCard>
-					<FeatureCard.Title>Courses</FeatureCard.Title>
-					<FeatureCard.Image
-						src="https://source.unsplash.com/random"
-						alt="Picture of the author"
-						width={125}
-						height={125}
-					/>
-				</FeatureCard>
-				<FeatureCard>
-					<FeatureCard.Title>Course Reviews</FeatureCard.Title>
-					<FeatureCard.Image
-						src="https://source.unsplash.com/random"
-						alt="Picture of the author"
-						width={125}
-						height={125}
-					/>
-				</FeatureCard>
-				<FeatureCard>
-					<FeatureCard.Title>Locations</FeatureCard.Title>
-					<FeatureCard.Image
-						src="https://source.unsplash.com/random"
-						alt="Picture of the author"
-						width={125}
-						height={125}
-					/>
-				</FeatureCard>
+			<div className="grid w-full grid-cols-2 gap-4 sm:grid-cols-3">
+				{features.map((feature) => (
+					<div className="rounded-lg">
+						<FeatureCard key={feature.title}>
+							<FeatureCard.Image
+								src={feature.image}
+								alt={feature.alt}
+							/>
+							{/* die div hier unten ist das Gradient */}
+							<div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden rounded-lg bg-gradient-to-t from-zinc-900 via-transparent" />
+							<FeatureCard.Title>{feature.title}</FeatureCard.Title>
+						</FeatureCard>
+					</div>
+				))}
 			</div>
 		</div>
 	);
--- src/frontend/components/feature-card/feature-card.tsx
+++ src/frontend/components/feature-card/feature-card.tsx
@@ -12,21 +12,20 @@ interface TitleProps {
 interface ImageProps {
 	src: string;
 	alt: string;
-	width: number;
-	height: number;
 }
 
 const Title: React.FC<TitleProps> = ({ children }) => {
-	return <h2>{children}</h2>;
+	return <h2 className="absolute bottom-0 mb-2 ml-2 text-lg font-medium text-background">{children}</h2>;
 };
 
-const ImageComponent: React.FC<ImageProps> = ({ src, alt, width, height }) => {
+const ImageComponent: React.FC<ImageProps> = ({ src, alt }) => {
 	return (
 		<Image
 			src={src}
 			alt={alt}
-			width={width}
-			height={height}
+			width={100}
+			height={100}
+			className="h-full w-full rounded-lg object-cover object-center"
 		/>
 	);
 };
@@ -35,7 +34,7 @@ const FeatureCard: React.FC<FeatureCardProps> & {
 	Title: React.FC<TitleProps>;
 	Image: React.FC<ImageProps>;
 } = ({ children }) => {
-	return <div className="flex flex-col">{children}</div>;
+	return <div className="relative h-36 w-full rounded-lg border">{children}</div>;
 };
 
 FeatureCard.Title = Title;
