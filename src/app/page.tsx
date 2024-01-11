785dd7bd84f4d61910b2dacc6efeda8ba054fb18
Andre Derjagin <derjagin.andre@gmail.com>
2024-01-11 10:57:35 +0100

    Update homepage content and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,11 +44,12 @@ export default function Home() {
 		<div className="flex flex-col gap-32 py-16 lg:pt-32">
 			<div className="flex flex-col gap-16 md:flex-row">
 				<div className="flex flex-col">
-					<h1 className="text-4xl font-medium">Introducing your University Helper</h1>
-					<p className="pb-8 pt-4 text-light">
-						Feeling Lost? Our Application helps you finding your place in on your university campus
+					<h1 className="text-4xl font-medium">Explore & Engage: Your University Companion!</h1>
+					<p className="pb-12 pt-6 text-light">
+						Navigate your campus, connect with peers, and find your place in university life - all with
+						ease!
 					</p>
-					<Button className="w-fit">Get started</Button>
+					<Button className="w-fit">Get started - it's free!</Button>
 				</div>
 				<Image
 					src="https://source.unsplash.com/random"
@@ -81,6 +82,14 @@ export default function Home() {
 
 			<div>
 				<Testimonials />
+			</div>
+
+			<div className="mx-auto flex w-full max-w-[680px] flex-col items-center text-center">
+				<h1 className="text-4xl font-medium">Explore & Engage: Your University Companion!</h1>
+				<p className="max-w-[480px] pb-12 pt-6 text-light">
+					Navigate your campus, connect with peers, and find your place in university life - all with ease!
+				</p>
+				<Button className="w-fit">Get started - it's free!</Button>
 			</div>
 		</div>
 	);
