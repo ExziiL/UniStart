ccbc371f025123b4cb8ea4974b4053135a85b1e7
Andre Derjagin <derjagin.andre@gmail.com>
2024-01-25 16:11:20 +0100

    Add settings page and components

--- src/frontend/components/settings-sidebar/settings-sidebar.tsx
+++ src/frontend/components/settings-sidebar/settings-sidebar.tsx
@@ -1,5 +1,15 @@
 import { Lock, Settings, User, Users } from "lucide-react";
+import Link from "next/link";
 import * as React from "react";
+
+interface SettingsSidebarProps {
+	sidebarItems?: {
+		icon: React.ReactNode;
+		name: string;
+		path: string;
+		component?: React.ReactNode;
+	}[];
+}
 
 const sidebarItems = [
 	{
@@ -30,14 +40,18 @@ function SettingsSidebar() {
 			<div className="mb-1 font-medium text-light">Settings</div>
 			<div>
 				<ul className="space-y-1">
-					{sidebarItems.map((item) => (
-						<li
-							className="flex cursor-pointer items-center space-x-2 rounded-md p-1 px-2 transition-colors hover:bg-zinc-200 dark:hover:bg-zinc-700"
+					{sidebarItems?.map((item) => (
+						<Link
+							href={item.path}
 							key={item.name}
 						>
-							<div>{item.icon}</div>
-							<div className="">{item.name}</div>
-						</li>
+							<li
+								className={`} flex cursor-pointer items-center space-x-2 rounded-md p-1 px-2 transition-colors hover:bg-zinc-200/60 dark:hover:bg-zinc-700`}
+							>
+								<div>{item.icon}</div>
+								<div className="">{item.name}</div>
+							</li>
+						</Link>
 					))}
 				</ul>
 			</div>
