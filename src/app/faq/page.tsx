8bad75fae43d6ce324f5ef9a02c28c1c4a551ac6
Andre Derjagin <derjagin.andre@gmail.com>
2024-02-01 10:48:13 +0100

    Add accordion component to FAQ page

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,7 +1,15 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/frontend/components/ui/accordion";
 import React from "react";
 
 function Faq() {
-	return <div>Faq</div>;
+	return (
+		<div>
+			<h1 className="text-3xl font-medium">Frequently Asked Questions</h1>
+			<p className="text-base text-light">
+				Looking for more information? Here are some things other students have asked us in the past.
+			</p>
+		</div>
+	);
 }
 
 export default Faq;
