61c4b206bd1f353eb23e75c809c2e032853ff67e
Andre Derjagin <derjagin.andre@gmail.com>
2024-02-01 11:39:00 +0100

    Add AiQuickChat component

---
+++ src/frontend/components/ai-quick-chat/ai-quick-chat.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AiQuickChat() {
+	return <div>AiQuickChat</div>;
+}
+
+export default AiQuickChat;
---
+++ src/frontend/components/ai-quick-chat/index.ts
@@ -0,0 +1,2 @@
+export * from './ai-quick-chat';
+export { default } from './ai-quick-chat';
