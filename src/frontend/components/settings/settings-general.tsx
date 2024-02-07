1b363c815b130ce1efc3ad3e515f8c3e0d7d6b6e
Andre Derjagin <derjagin.andre@gmail.com>
2024-02-07 09:09:16 +0100

    Add delete account functionality to general settings

--- src/frontend/components/settings/settings-general.tsx
+++ src/frontend/components/settings/settings-general.tsx
@@ -9,7 +9,17 @@ function SettingsGeneral() {
 		<div className="mx-auto w-full max-w-[960px] space-y-8 p-4">
 			<h1 className="pt-6 text-xl font-semibold text-primary">General Settings</h1>
 			<Separator />
-			<div className="space-y-8"></div>
+			<div className="">
+				<h2 className="text-lg font-semibold text-destructive">Delete account</h2>
+				<p className="mb-4 mt-1">Once you delete your account, there is no going back. Please be certain.</p>
+				<Button variant="destructive">
+					<Trash2
+						size={18}
+						className="mr-2"
+					/>
+					Delete your account
+				</Button>
+			</div>
 		</div>
 	);
 }
