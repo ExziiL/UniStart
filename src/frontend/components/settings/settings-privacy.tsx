e6dc524b6c1fb16120ab88d2f150060404939b65
Andre Derjagin <derjagin.andre@gmail.com>
2024-02-08 12:07:06 +0100

    Add checkbox component and settings to settings/privacy page

--- src/frontend/components/settings/settings-privacy.tsx
+++ src/frontend/components/settings/settings-privacy.tsx
@@ -1,3 +1,4 @@
+import { Checkbox } from "@/frontend/components/ui/checkbox";
 import { Separator } from "@/frontend/components/ui/separator";
 import React from "react";
 
@@ -8,7 +9,35 @@ function SettingsPrivacy() {
 				<h2 className="text-lg font-semibold text-destructive">Activity</h2>
 				<p className="mb-4 mt-1">Once you delete your account, there is no going back. Please be certain.</p>
 			</div>
-			<div>...</div>
+			<div className="space-y-8">
+				<div className="flex items-center space-x-2">
+					<Checkbox id="profilePic" />
+					<label
+						htmlFor="profilePic"
+						className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+					>
+						Hide Profile Picture
+					</label>
+				</div>
+				<div className="flex items-center space-x-2">
+					<Checkbox id="onlineStatus" />
+					<label
+						htmlFor="onlineStatus"
+						className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+					>
+						Hide Online Status
+					</label>
+				</div>
+				<div className="flex items-center space-x-2">
+					<Checkbox id="friendRequests" />
+					<label
+						htmlFor="friendRequests"
+						className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+					>
+						Disable Friend Requests
+					</label>
+				</div>
+			</div>
 		</div>
 	);
 }
