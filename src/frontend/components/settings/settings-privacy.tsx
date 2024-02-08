3d6e3d058df79d65257073401a943ec4ce63fa3f
Andre Derjagin <derjagin.andre@gmail.com>
2024-02-08 12:28:51 +0100

    Remove activity section from settings privacy

--- src/frontend/components/settings/settings-privacy.tsx
+++ src/frontend/components/settings/settings-privacy.tsx
@@ -5,10 +5,6 @@ import React from "react";
 function SettingsPrivacy() {
 	return (
 		<div className="mx-auto w-full max-w-[960px] space-y-8">
-			<div className="">
-				<h2 className="text-lg font-semibold text-destructive">Activity</h2>
-				<p className="mb-4 mt-1">Once you delete your account, there is no going back. Please be certain.</p>
-			</div>
 			<div className="space-y-8">
 				<div className="flex items-center space-x-2">
 					<Checkbox id="profilePic" />
