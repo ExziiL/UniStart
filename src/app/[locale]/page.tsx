d1fbded4f7bfb7abce7a3f829bd4fb765a1e4df0
Andre Derjagin <derjagin.andre@gmail.com>
2024-02-10 14:17:14 +0100

    Enhanced homepage with dynamic i18n content

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,8 @@
-"use client";
-
 import Features from "@/frontend/components/features";
 import Testimonials from "@/frontend/components/testimonials";
 import { Button } from "@/frontend/components/ui/button";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
-import { Link } from "../../navigation";
 
 export default function Home() {
 	const t = useTranslations("Home");
@@ -14,32 +11,13 @@ export default function Home() {
 		<div className="full-bleed flex flex-col items-center gap-32 px-4 py-16 lg:pt-32">
 			<div className="flex max-w-[1024px] flex-col gap-16 md:flex-row">
 				<div className="flex flex-col">
-					{/* ---------------- */}
-					<div>{t("title")}</div>
-					<Link
-						href="/lectures"
-						locale="en"
-					>
-						Go to Lectures
-					</Link>
-					<Link
-						href="/lectures"
-						locale="de"
-					>
-						Zu den Vorlesungen
-					</Link>
-					{/* ---------------- */}
-
-					<h1 className="text-4xl font-medium">Explore & Engage: Your University Companion!</h1>
-					<p className="pb-12 pt-6 text-light">
-						Navigate your campus, connect with peers, and find your place in university life - all with
-						ease!
-					</p>
-					<Button className="w-fit">Get started - it's free!</Button>
+					<h1 className="text-4xl font-medium">{t("title")}</h1>
+					<p className="pb-12 pt-6 text-light">{t("sub-heading")}</p>
+					<Button className="w-fit">{t("button")}</Button>
 				</div>
 				<Image
 					src="https://source.unsplash.com/random"
-					alt="Picture of the author"
+					alt={t("image-alt")}
 					width={400}
 					height={400}
 					style={{ borderRadius: "8px", objectFit: "fill" }}
@@ -53,11 +31,9 @@ export default function Home() {
 			</div>
 
 			<div className="mx-auto flex w-full max-w-[680px] flex-col items-center text-center">
-				<h1 className="text-4xl font-medium">Explore & Engage: Your University Companion!</h1>
-				<p className="max-w-[480px] pb-12 pt-6 text-light">
-					Navigate your campus, connect with peers, and find your place in university life - all with ease!
-				</p>
-				<Button className="w-fit">Get started - it's free!</Button>
+				<h1 className="text-4xl font-medium">{t("title")}</h1>
+				<p className="max-w-[480px] pb-12 pt-6 text-light">{t("sub-heading")}</p>
+				<Button className="w-fit">{t("button")}</Button>
 			</div>
 		</div>
 	);
--- src/frontend/components/features/features.tsx
+++ src/frontend/components/features/features.tsx
@@ -1,46 +1,49 @@
 import FeatureCardWrapper from "@/frontend/components/feature-card-wrapper";
+import { useTranslations } from "next-intl";
 import * as React from "react";
 
-const features = [
-	{
-		title: "Course Overviews",
-		image: "https://source.unsplash.com/random",
-		alt: "Picture of the author",
-		href: "/vorlesungen",
-	},
-	{
-		title: "Course Reviews",
-		image: "https://source.unsplash.com/random",
-		alt: "Picture of the author",
-		href: "/vorlesungen/grundlagen-der-mathematik",
-	},
-	{
-		title: "Locations",
-		image: "https://source.unsplash.com/random",
-		alt: "Picture of the author",
-		href: "/map",
-	},
-	{
-		title: "Chat",
-		image: "https://source.unsplash.com/random",
-		alt: "Picture of the author",
-		href: "/chat",
-	},
-	{
-		title: "Ai-Chat",
-		image: "https://source.unsplash.com/random",
-		alt: "Picture of the author",
-		href: "/ai-chat",
-	},
-	{
-		title: "Frequently Asked Questions",
-		image: "https://source.unsplash.com/random",
-		alt: "Picture of the author",
-		href: "/faq",
-	},
-];
-
 function Features() {
+	const t = useTranslations("Home.features");
+
+	const features = [
+		{
+			title: t("feature1.title"),
+			image: "https://source.unsplash.com/random",
+			alt: t("feature1.alt"),
+			href: "/vorlesungen",
+		},
+		{
+			title: t("feature2.title"),
+			image: "https://source.unsplash.com/random",
+			alt: t("feature1.alt"),
+			href: "/vorlesungen/grundlagen-der-mathematik",
+		},
+		{
+			title: t("feature3.title"),
+			image: "https://source.unsplash.com/random",
+			alt: t("feature3.alt"),
+			href: "/map",
+		},
+		{
+			title: t("feature4.title"),
+			image: "https://source.unsplash.com/random",
+			alt: t("feature4.alt"),
+			href: "/chat",
+		},
+		{
+			title: t("feature5.title"),
+			image: "https://source.unsplash.com/random",
+			alt: t("feature5.alt"),
+			href: "/ai-chat",
+		},
+		{
+			title: t("feature6.title"),
+			image: "https://source.unsplash.com/random",
+			alt: t("feature6.alt"),
+			href: "/faq",
+		},
+	];
+
 	return (
 		<div className="grid w-full max-w-[1024px] grid-cols-2 gap-6 sm:grid-cols-3">
 			{features.map((feature) => (
