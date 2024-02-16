11096d31394e8e0264793665a34a2dd6ce9f6fff
Andre Derjagin <derjagin.andre@gmail.com>
2024-02-16 13:59:22 +0100

    fixed badge colors on dark mode

--- src/frontend/components/course-badge/course-badge.tsx
+++ src/frontend/components/course-badge/course-badge.tsx
@@ -1,22 +1,22 @@
-import { Badge, BadgeProps } from '@/frontend/components/ui/badge';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/frontend/components/ui/tooltip';
-import { cn } from '@/lib/utils';
-import { cva, type VariantProps } from 'class-variance-authority';
-import React from 'react';
+import { Badge, BadgeProps } from "@/frontend/components/ui/badge";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/frontend/components/ui/tooltip";
+import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+import React from "react";
 
-const badgeDifficulty = cva('', {
+const badgeDifficulty = cva("", {
 	variants: {
 		variant: {
-			none: '',
-			easy: 'bg-green-100 text-green-500 border-green-500 hover:bg-green-200 hover:text-green-600',
-			medium: 'bg-yellow-100 text-yellow-500 border-yellow-500 hover:bg-yellow-200 hover:text-yellow-600 dark:bg-yellow-50 dark:text-yellow-600 dark:border-yellow-600 dark:hover:bg-yellow-200 dark:hover:text-yellow-700',
-			hard: 'bg-red-100 text-red-500 border-red-500 hover:bg-red-200 hover:text-red-600',
+			none: "",
+			easy: "bg-green-100 text-green-500 border-green-500 hover:bg-green-200 dark:bg-green-800 dark:text-green-400 dark:hover:bg-green-900",
+			medium: "bg-yellow-100 text-yellow-500 border-yellow-500 hover:bg-yellow-200 dark:bg-yellow-800 dark:text-yellow-400 dark:border-yellow-600 dark:hover:bg-yellow-900",
+			hard: "bg-red-100 text-red-500 border-red-500 dark:bg-red-800/50 hover:bg-red-200 dark:hover:bg-red-900/80 dark:text-red-400",
 		},
 	},
 });
 
 interface CourseBadgeProps extends BadgeProps {
-	difficulty: 'easy' | 'medium' | 'hard';
+	difficulty: "easy" | "medium" | "hard";
 }
 
 function CourseBadge({ className, difficulty }: CourseBadgeProps) {
@@ -25,7 +25,13 @@ function CourseBadge({ className, difficulty }: CourseBadgeProps) {
 			<TooltipProvider>
 				<Tooltip>
 					<TooltipTrigger>
-						<Badge className={cn(badgeDifficulty({ variant: difficulty }), className, 'capitalize')}>
+						<Badge
+							className={cn(
+								badgeDifficulty({ variant: difficulty }),
+								className,
+								"cursor-default capitalize"
+							)}
+						>
 							{difficulty}
 						</Badge>
 					</TooltipTrigger>
