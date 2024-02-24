dc26fdadc0cf5494a32daf1510ce82ac80542598
Andre Derjagin <derjagin.andre@gmail.com>
2024-02-24 10:28:43 +0100

    added error and loading state to terminplan

--- src/app/terminplan/page.tsx
+++ src/app/terminplan/page.tsx
@@ -1,33 +1,83 @@
-import React from "react";
+"use client";
 
 import AppointmentCard from "@/frontend/components/appointment-card";
 import AppointmentTable from "@/frontend/components/appointment-table";
-import { Download } from "lucide-react";
-
-const eintraege = [
-	{
-		title: "Infoveranstaltung für Masterstudiengänge",
-		date: "Montag, 10.07.2023",
-		dueDay: "fällig in 9 Tagen",
-	},
-	{
-		title: "Infoveranstaltung für Bachelorstudiengänge",
-		date: "Mittwoch, 12.07.2023",
-		dueDay: "fällig in 12 Tagen",
-	},
-	{
-		title: "Bewerbungsschluss für Masterstudiengänge",
-		date: "Samstag, 15.07.2023",
-		dueDay: "fällig in 16 Tagen",
-	},
-	{
-		title: "Letzter Termin zur Immatrikulation bei NC-freien Bachelorstudiengängen und höheren Semestern	",
-		date: "Samstag, 06.10.2023",
-		dueDay: "fällig in 43 Tagen",
-	},
-];
+import { fetchAppointments } from "@/frontend/components/appointment-table/actions";
+import { Button } from "@/frontend/components/ui/button";
+import { Skeleton } from "@/frontend/components/ui/skeleton";
+import { Infos } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 function Terminplan() {
+	const router = useRouter();
+	const { isPending, isError, error, data } = useQuery({
+		queryKey: ["webscrapper"],
+		queryFn: fetchAppointments,
+		retry: false,
+	});
+
+	if (isPending) {
+		return (
+			<div className="mx-auto max-w-[1024px] space-y-16">
+				<Skeleton className="my-16 h-16 w-[1000px]" />
+
+				<div className="space-y-6">
+					<div className="flex items-end gap-4">
+						<Skeleton className="h-10 w-[425px]" />
+					</div>
+					<div className="grid grid-cols-2 gap-6">
+						<Skeleton className="h-[150px] w-full" />
+						<Skeleton className="h-[150px] w-full" />
+						<Skeleton className="h-[150px] w-full" />
+						<Skeleton className="h-[150px] w-full" />
+					</div>
+				</div>
+
+				<div className="space-y-6 pb-16">
+					<Skeleton className="h-12 w-[400px]" />
+
+					<div>
+						<Skeleton className="h-[600px] w-full" />
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	if (isError) {
+		return (
+			<div className="flex h-full flex-col justify-center pb-60">
+				<div className="text-4xl font-medium">
+					<div>Oh no,</div>
+					<p>Something went wrong!</p>
+				</div>
+				<div className="pt-4 text-light">Please refresh the page and try again</div>
+				<div className="space-x-2">
+					<Button
+						variant="default"
+						className="mt-8"
+						onClick={() => router.refresh()}
+					>
+						Refresh
+					</Button>
+					<Button
+						variant="ghost"
+						className="mt-8"
+						onClick={() => router.back()}
+					>
+						Back
+					</Button>
+				</div>
+			</div>
+		);
+	}
+
+	const entries: Infos[] = data?.appointments;
+
+	const firstFourEntries = entries.slice(0, 4);
+
 	return (
 		<div className="mx-auto max-w-[1024px] space-y-16">
 			<h1 className="my-16 text-6xl font-bold text-primary">Terminplan für das WS 2023/2024</h1>
@@ -35,17 +85,12 @@ function Terminplan() {
 			<div className="space-y-6">
 				<div className="flex items-end gap-4">
 					<h2 className="text-4xl font-medium text-primary">Upcoming Appointments</h2>
-					<span className="cursor-pointer pb-1 text-light transition-colors hover:text-primary">
-						<Download className="h-6 w-6" />
-					</span>
 				</div>
 				<div className="grid grid-cols-2 gap-6">
-					{eintraege.map((eintrag) => (
+					{firstFourEntries.map((entry) => (
 						<AppointmentCard
-							key={eintrag.title + eintrag.date}
-							title={eintrag.title}
-							dueDay={eintrag.dueDay}
-							date={eintrag.date}
+							key={entry.title}
+							entry={entry}
 						/>
 					))}
 				</div>
--- src/frontend/components/appointment-card/appointment-card.tsx
+++ src/frontend/components/appointment-card/appointment-card.tsx
@@ -1,30 +1,31 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent } from "react";
 
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/frontend/components/ui/card';
-import { CalendarDays } from 'lucide-react';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/frontend/components/ui/card";
+import { Infos } from "@prisma/client";
+import { CalendarDays } from "lucide-react";
 
 type AppointmentCardProps = {
-	title: string;
-	date: string;
-	dueDay: string;
+	entry: Infos;
+	// date: string;
 };
 
-const AppointmentCard: FunctionComponent<AppointmentCardProps> = ({ title, date, dueDay }) => {
+const AppointmentCard: FunctionComponent<AppointmentCardProps> = ({ entry }) => {
+	console.log(entry.dates);
 	return (
 		<Card className="flex flex-col justify-between shadow-sm">
 			<CardHeader className="p-4 pb-0">
-				<CardTitle className="text-base text-primary">{title}</CardTitle>
+				<CardTitle className="text-base text-primary">{entry.title}</CardTitle>
 			</CardHeader>
 			<div>
 				<CardContent className="px-4 pb-3 pt-1">
-					<p className="text-base font-light text-primary">{dueDay}</p>
+					{/* <p className="text-base font-light text-primary">{dueDay}</p> */}
 				</CardContent>
 				<CardFooter className="space-x-1 p-4 pt-0 text-light">
 					<CalendarDays
 						size={20}
 						strokeWidth={1.75}
 					/>
-					<p className="text-sm">{date}</p>
+					<p className="text-sm">{entry.dates}</p>
 				</CardFooter>
 			</div>
 		</Card>
