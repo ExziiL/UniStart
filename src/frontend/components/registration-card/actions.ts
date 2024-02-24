4dbba410ec6dd26beba2d60d37c1fb5c2f1465d1
Andre Derjagin <derjagin.andre@gmail.com>
2024-02-24 17:35:00 +0100

    add return message if user already exist and format with prettier

--- src/frontend/components/registration-card/actions.ts
+++ src/frontend/components/registration-card/actions.ts
@@ -1,68 +1,71 @@
-import { User } from "next-auth";
-import { signIn } from "next-auth/react";
-import * as z from "zod";
-import { registrationFormSchema } from "./registration-form";
+import { User } from 'next-auth';
+import { signIn } from 'next-auth/react';
+import * as z from 'zod';
+import { registrationFormSchema } from './registration-form';
 
 export async function register(action: string, values: z.infer<typeof registrationFormSchema> | null) {
-	let response;
-	switch (action) {
-		case "github":
-		case "google":
-			response = await signIn(action, { redirect: false });
-			break;
-		case "credentials":
-			response = await emailRegister(values);
-			break;
-		default:
-			console.error("Invalid action");
-			break;
-	}
+   
+    let response;
+    switch (action) {
+        case 'github': case 'google':
+            response = await signIn(action, { redirect: false });
+            break;
+        case 'credentials':
+            response = await emailRegister(values);
+            break;
+        default:
+            console.error('Invalid action');
+            break;
+    }
 
-	return response;
+    return response;
 }
 
 async function emailRegister(values: z.infer<typeof registrationFormSchema> | null) {
-	if (!values) throw new Error("Invalid values");
+    if (!values) throw new Error('Invalid values');
 
-	const userExists = await fetch("api/register?" + new URLSearchParams({ email: values.email }), {
-		method: "GET",
-		headers: { "Content-Type": "application/json" },
-	});
+    const userExists = await fetch('api/register?' +
+        new URLSearchParams({ email: values.email }), {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    });
 
-	const { user } = await userExists.json();
+    const { user } = await userExists.json();
 
-	if (user) {
-		console.error("user already exists");
-		return { ok: false, userExists: true };
-	}
+    if (user) {
+        console.error("user already exists");
+        return { ok: false, userExists: true };
+    }
 
-	const response = await fetch("api/register", {
-		method: "PUT",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({
-			name: values.username,
-			email: values.email,
-			password: values.password,
-		}),
-	});
+    const response = await fetch('api/register', {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(
+            {
+                name: values.username,
+                email: values.email,
+                password: values.password
+            })
+    })
 
-	if (response.ok) {
-		const { user } = await response.json();
-		// console.log(user);
+    if (response.ok) {
+        const { user } = await response.json()
+        // console.log(user);
 
-		await createNode(user);
-	}
-	return response;
+        await createNode(user);
+    }
+    return response;
 }
 
-export async function createNode(user: User, hostUrl = "") {
-	const res = await fetch(hostUrl + "api/graph", {
-		method: "PUT",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({ email: user?.email }),
-	});
 
-	if (!res.ok) {
-		throw new Error("User Cretaion in Graph didn't work properly\n" + (await res.json()));
-	}
+export async function createNode(user: User, hostUrl = "") {
+    const res = await fetch(hostUrl + 'api/graph', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email: user?.email })
+    });
+
+    if (!res.ok) {
+        throw new Error("User Cretaion in Graph didn't work properly\n" + await res.json());
+    }
 }
