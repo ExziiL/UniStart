83338e20be3ec5d024b5f36c7723d16d85ca10fb
Andre Derjagin <derjagin.andre@gmail.com>
2024-02-24 19:33:11 +0100

    add y-padding to vorlesungen page

--- src/app/vorlesungen/page.tsx
+++ src/app/vorlesungen/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Vorlesungen() {
 	return (
 		// <div className="grid gap-4 p-4 md:grid-cols-4 lg:p-0 lg:py-4">
-		<div className="flex flex-col gap-10">
+		<div className="flex flex-col gap-10 py-12">
 			<SemesterCarousel semester={1} />
 			<SemesterCarousel semester={2} />
 			<SemesterCarousel semester={3} />
