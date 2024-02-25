f0179d966eafe678584063ae704f4e7a42d93531
Andre Derjagin <derjagin.andre@gmail.com>
2024-02-25 09:01:43 +0100

    lint on new-conversation-user-chat

--- src/frontend/components/new-conversation-user-chat/new-conversation-user-chat.tsx
+++ src/frontend/components/new-conversation-user-chat/new-conversation-user-chat.tsx
@@ -5,20 +5,20 @@ import React from "react";
 interface NewConversationUserChatProps extends React.HTMLAttributes<HTMLDivElement> {
 	// TODO: change any to user type
 	user?: IUser;
-};
+}
 
 // TODO: change static data to user data
-function NewConversationUserChat({ user}: NewConversationUserChatProps) {
+function NewConversationUserChat({ user }: NewConversationUserChatProps) {
 	const [isSelected, setIsSelected] = React.useState(false);
 
 	return (
 		<div
-			className={`${isSelected ? "bg-ultra-light/15" : ""
-				} my-1 flex w-full cursor-pointer flex-row gap-4 rounded-sm px-3 pt-4 hover:bg-ultra-light/10`}
+			className={`${
+				isSelected ? "bg-ultra-light/15" : ""
+			} my-1 flex w-full cursor-pointer flex-row gap-4 rounded-sm px-3 pt-4 hover:bg-ultra-light/10`}
 			onClick={() => {
-				setIsSelected(!isSelected)
-			}
-			}
+				setIsSelected(!isSelected);
+			}}
 		>
 			<div className="relative">
 				<Avatar>
