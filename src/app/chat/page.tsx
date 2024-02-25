1949228ed25ddeb0e69c0e497a98800950ad981d
Andre Derjagin <derjagin.andre@gmail.com>
2024-02-25 10:09:50 +0100

    animate user-bar in chat

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useUserContext } from "@/context/user-context/user-context";
 import UserBar from "@/frontend/components/user-bar/user-bar";
 import UserChatBox from "@/frontend/components/user-chat-box";
 import UserConversations from "@/frontend/components/user-conversations";
 import { message } from "@prisma/client";
+import { motion } from "framer-motion";
 import React from "react";
 
 function Chat() {
@@ -14,7 +14,16 @@ function Chat() {
 	return (
 		<div className="full-bleed grid grid-cols-1 grid-rows-[auto,_1fr]">
 			<div className="col-span-2 row-auto 2xl:col-span-3">
-				<UserBar className="" />
+				{messages?.length != 0 && (
+					<motion.div
+						initial={{ y: -100, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						exit={{ y: -100, opacity: 0 }}
+						transition={{ type: "spring", stiffness: 50 }}
+					>
+						<UserBar className="" />
+					</motion.div>
+				)}
 			</div>
 			{/* <div className="flex flex-row"> */}
 			{/* TODO: UserCHatMessages ab md: anzeigen, davor anders darstellen */}
