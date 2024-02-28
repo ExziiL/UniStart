cb900b771f27abce5fc933612f90b0884e654bb3
Andre Derjagin <derjagin.andre@gmail.com>
2024-02-28 17:49:42 +0100

    removed

--- src/app/vorlesungen/[vorlesung]/page.tsx
+++ src/app/vorlesungen/[vorlesung]/page.tsx
@@ -20,16 +20,7 @@ function Page({ params }: PageProps) {
 
 	if (!vorlesung) {
 		// TODO: add (custom) 404 page
-		return (
-			<>
-				<h1 className="text-xl font-semibold text-primary">404 - Vorlesung Not Found</h1>
-				<p className="text-primary">{`Oops! The page you're looking for doesn't exist.`}</p>
-				<p className="text-primary">{`You may want to head back to the homepage. If you think something is broken, report a problem.`}</p>
-				<div>
-					<Link href="/">Go Home</Link>
-				</div>
-			</>
-		);
+		return <div></div>;
 	}
 
 	return (
