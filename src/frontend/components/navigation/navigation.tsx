07d8fc2075d884950bd2f098f37d17f3870604b0
Andre Derjagin <derjagin.andre@gmail.com>
2024-02-28 16:33:31 +0100

    Add "Not logged in" announcement

--- src/frontend/components/navigation/navigation.tsx
+++ src/frontend/components/navigation/navigation.tsx
@@ -1,16 +1,48 @@
 "use client";
 
-import React from "react";
-
 import { useUserContext } from "@/context/user-context/user-context";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
 import MainNav from "./main-nav";
 import MobileNav from "./mobile-nav";
 
 const Navigation = () => {
 	const { userState } = useUserContext();
+	const session = useSession();
 
 	const isUserStateEmpty = Object.keys(userState).length === 0;
-	console.log("Is userState empty?", isUserStateEmpty);
+
+	const [showNotLoggedInMessage, setShowNotLoggedInMessage] = React.useState(false);
+
+	React.useEffect(() => {
+		const timer = setTimeout(() => {
+			setShowNotLoggedInMessage(true);
+		}, 1000);
+
+		return () => clearTimeout(timer);
+	}, []);
+
+	const NotLoggedIn = () => {
+		return (
+			showNotLoggedInMessage &&
+			(session.status !== "authenticated" || isUserStateEmpty || userState.name === "") && (
+				<div className="flex h-10 w-full flex-row items-center justify-center border-b border-red-100 bg-red-100/65 text-sm text-light shadow-sm shadow-red-200/25 dark:border-red-900/30 dark:bg-red-800/15 dark:shadow-red-600/25">
+					<div>
+						You are currently not logged in. Please{" "}
+						<span className="pr-1 text-link hover:cursor-pointer hover:underline">
+							<Link href="/login">Log In</Link>
+						</span>
+						or
+						<span className="px-1 text-link hover:cursor-pointer hover:underline">
+							<Link href="/registration">Sign Up</Link>
+						</span>
+						to unlock all the functionality.
+					</div>
+				</div>
+			)
+		);
+	};
 
 	return (
 		<>
@@ -21,9 +53,7 @@ const Navigation = () => {
 				</div>
 			</header>
 
-			{(isUserStateEmpty || userState.name == "") && (
-				<div>You are currently not logged in. Please log in to get all the functionality.</div>
-			)}
+			<NotLoggedIn />
 		</>
 	);
 };
