199e0f8f98c9aa892333e841ea0a28cd33b00533
Andre Derjagin <derjagin.andre@gmail.com>
2024-02-28 23:42:47 +0100

    idk anymore

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -1,13 +1,7 @@
 import RegistrationCard from "@/frontend/components/registration-card";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 import React from "react";
-import { authOptions } from "../api/auth/[...nextauth]/route";
 
 async function Registration() {
-	const session = await getServerSession(authOptions);
-
-	//if (session) redirect('/');
 	return <RegistrationCard />;
 }
 
