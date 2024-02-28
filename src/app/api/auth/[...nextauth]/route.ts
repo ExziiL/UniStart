39883e5d065ea2194050509d0fc02588a7475e94
Andre Derjagin <derjagin.andre@gmail.com>
2024-02-28 16:33:45 +0100

    formatting on route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,88 +1,89 @@
-import NextAuth, { AuthOptions } from "next-auth";
+import prisma from "@/backend/lib/prisma";
+import { setOnlineState } from "@/frontend/components/login-card/actions";
+import { createNode } from "@/frontend/components/registration-card/actions";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import bcrypt from "bcryptjs";
+import NextAuth, { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import bcrypt from "bcryptjs"
-import prisma from "@/backend/lib/prisma";
-import { createNode } from "@/frontend/components/registration-card/actions";
-import { setOnlineState } from "@/frontend/components/login-card/actions";
 
 export const authOptions: AuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-        }),
-        CredentialsProvider({
-            name: "credentials",
-            credentials: {
-                username_email: { label: "email", type: "text" },
-                password: { label: "password", type: "password" }
-            },
-            async authorize(credentials) {
-                if (!credentials?.username_email || !credentials?.password) {
-                    throw new Error("Invalid credentials");
-                }
-                const { username_email, password } = credentials;
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		GitHubProvider({
+			clientId: process.env.GITHUB_ID as string,
+			clientSecret: process.env.GITHUB_SECRET as string,
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		}),
+		CredentialsProvider({
+			name: "credentials",
+			credentials: {
+				username_email: { label: "email", type: "text" },
+				password: { label: "password", type: "password" },
+			},
+			async authorize(credentials) {
+				if (!credentials?.username_email || !credentials?.password) {
+					throw new Error("Invalid credentials");
+				}
+				const { username_email, password } = credentials;
 
-                try {
-                    const user = await prisma.user.findFirst({
-                        where: {
-                            OR: [
-                                { email: username_email },
-                                { name: username_email }
-                            ]
-                        }
-                    })
+				try {
+					const user = await prisma.user.findFirst({
+						where: {
+							OR: [{ email: username_email }, { name: username_email }],
+						},
+					});
 
-                    if (!user || !user?.password) {
-                        throw new Error("Invalide credentials")
-                    }
-                    const passwordMatch = await bcrypt.compare(
-                        password,
-                        user.password
-                    );
+					if (!user || !user?.password) {
+						throw new Error("Invalide credentials");
+					}
+					const passwordMatch = await bcrypt.compare(password, user.password);
 
-                    if (!passwordMatch) {
-                        throw new Error("Invalide credentials");
-                    }
+					if (!passwordMatch) {
+						throw new Error("Invalide credentials");
+					}
 
-                    return user;
-                } catch (error) {
-                    throw new Error("Something went wrong: \n" + error)
-                }
-            },
-        }),
-    ],
-    debug: process.env.NODE_ENV === 'development',
-    events: {
-        signIn: async (data) => {
-            const online = true;
-            await setOnlineState(data.user.email!, online);
-        },
-        signOut: async (data) => {
-            const online = false;
-            await setOnlineState(data.session.user?.email!, online)
-        },
-        createUser: async (data) => {
-            const baseUrl = process.env.VERCEL_URL ?? process.env.NEXTAUTH_URL!
-            await createNode(data.user, baseUrl);
-        },
-    },
-    session: {
-        strategy: "jwt",
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    pages: {
-        signIn: "/"
-    }
+					return user;
+				} catch (error) {
+					throw new Error("Something went wrong: \n" + error);
+				}
+			},
+		}),
+	],
+	debug: process.env.NODE_ENV === "development",
+	events: {
+		signIn: async (data) => {
+			const online = true;
+			await setOnlineState(data.user.email!, online);
+		},
+		signOut: async (data) => {
+			const online = false;
+			try {
+				if (!data.session.user?.email) {
+					throw new Error("User email not available");
+				}
+				await setOnlineState(data.session.user.email, online);
+			} catch (error) {
+				console.error("Error setting user offline state:", error);
+			}
+		},
+		createUser: async (data) => {
+			const baseUrl = process.env.VERCEL_URL ?? process.env.NEXTAUTH_URL!;
+			await createNode(data.user, baseUrl);
+		},
+	},
+	session: {
+		strategy: "jwt",
+	},
+	secret: process.env.NEXTAUTH_SECRET,
+	pages: {
+		signIn: "/",
+	},
 };
 
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
